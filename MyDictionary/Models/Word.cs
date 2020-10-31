8d031a4cbf965af23ee0d95a0979709245797465
Wojtek Krol <wojtekmkrol@gmail.com>
2020-10-31 21:14:42 +0100

    Dodaj pliki projektów.

---
+++ MyDictionary/Models/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDictionary.Models
+{
+    public class Word
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string PolishWord { get; set; }
+
+        [Required]
+        public string FrenchWord { get; set; }
+    }
+}
